d2704659530e620069cec19539bb8eda93c304b3
Pyae Sone Shin Thant <pssthant@gmail.com>
2026-01-16 14:18:41 +0700

    feat(App.tsx): add EventAttendees page and route, refactor imports and JSX formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import {
-  Routes,
-  Route,
-  BrowserRouter as Router
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
-import RegisterPage from './pages/Register';
-import Home from './pages/Home';
-import CreatedEvents from './pages/CreatedEvents';
-import EventDetail from './pages/EventDetail';
-import NavBar from './components/NavBar';
-import Profile from './pages/Profile';
-import AddEvent from './pages/AddEvent';
-import AttendedEvents from './pages/AttendedEvents';
+import RegisterPage from "./pages/Register";
+import Home from "./pages/Home";
+import CreatedEvents from "./pages/CreatedEvents";
+import EventDetail from "./pages/EventDetail";
+import NavBar from "./components/NavBar";
+import Profile from "./pages/Profile";
+import AddEvent from "./pages/AddEvent";
+import AttendedEvents from "./pages/AttendedEvents";
+import EventAttendees from "./pages/EventAttendees";
 function App() {
-
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
@@ -22,39 +18,19 @@ function App() {
         </div>
         <div className="flex-1">
           <Routes>
-            <Route 
-              path="/"
-              element={<Home />}
-            />
-            <Route 
-              path="/register"
-              element={<RegisterPage />}
-            />
-            <Route 
-              path="/profile"
-              element={<Profile />}
-            />
-              <Route
-              path="/created-events"
-              element={<CreatedEvents />}
-            />
-            <Route 
-              path="/attended-events"
-              element={<AttendedEvents />}
-            />
-            <Route 
-              path="/event/:id"
-              element={<EventDetail />}
-            />
-            <Route 
-              path="/add-event/"
-              element={<AddEvent />}
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/created-events" element={<CreatedEvents />} />
+            <Route path="/attended-events" element={<AttendedEvents />} />
+            <Route path="/event/:id" element={<EventDetail />} />
+            <Route path="/add-event/" element={<AddEvent />} />
+            <Route path="/event/:id/attendees" element={<EventAttendees />} />
           </Routes>
         </div>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
