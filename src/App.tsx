1b19b2f757793f4876e3ebb3c01f5dc3292a6944
Pyae Sone Shin Thant <pssthant@gmail.com>
2026-01-16 19:13:55 +0700

    feat(App.tsx): wrap app routes with AuthProvider to enable authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,31 @@ import Profile from "./pages/Profile";
 import AddEvent from "./pages/AddEvent";
 import AttendedEvents from "./pages/AttendedEvents";
 import EventAttendees from "./pages/EventAttendees";
+import { AuthProvider } from "./context/AuthContext";
+
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <div className="sticky top-0 z-50">
-          <NavBar />
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen flex flex-col">
+          <div className="sticky top-0 z-50">
+            <NavBar />
+          </div>
+          <div className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/created-events" element={<CreatedEvents />} />
+              <Route path="/attended-events" element={<AttendedEvents />} />
+              <Route path="/event/:id" element={<EventDetail />} />
+              <Route path="/add-event/" element={<AddEvent />} />
+              <Route path="/event/:id/attendees" element={<EventAttendees />} />
+            </Routes>
+          </div>
         </div>
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/created-events" element={<CreatedEvents />} />
-            <Route path="/attended-events" element={<AttendedEvents />} />
-            <Route path="/event/:id" element={<EventDetail />} />
-            <Route path="/add-event/" element={<AddEvent />} />
-            <Route path="/event/:id/attendees" element={<EventAttendees />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
